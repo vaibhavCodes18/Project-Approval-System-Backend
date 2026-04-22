9fd2575464356a5c52e10c1c40265179b596c879
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-04-22 23:59:48 +0530

    Everything upto date

--- src/test/java/com/bit/ProjectApprovalSystem/service/ProjectServiceTest.java
+++ src/test/java/com/bit/ProjectApprovalSystem/service/ProjectServiceTest.java
@@ -1,4 +1,24 @@
 package com.bit.ProjectApprovalSystem.service;
 
-public class ProjectServiceTest {
+import com.bit.ProjectApprovalSystem.repository.ProjectRepository;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class ProjectServiceTest {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+    @Disabled
+    @Test
+    void testProject(){
+        assertEquals(19, 9+10);
+        assertNotNull(projectRepository.findById(new ObjectId("69dcaeaa69ebc60560958ebf")));
+    }
+
 }
